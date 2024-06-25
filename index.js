2e80ad17ca7b44b6ef3b3a5ee86435eeb8397651
kamesan1577 <kamesan1577@users.noreply.github.com>
2024-06-26 02:31:37 +0900

    chore: Refactor artifact client initialization in index.js

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ const fs = require('fs');
 const path = require('path');
 
 async function run() {
-    try {
-        const artifactName = core.getInput('artifact-name');
-        const artifactClient = new artifact.DefaultArtifactClient();
-        const downloadResponse = await artifactClient.downloadArtifact(artifactName);
-        const filePath = path.join(downloadResponse.downloadPath, 'jest-results.json');
+    // try {
+    const artifactName = core.getInput('artifact-name');
+    const artifactClient = new artifact.DefaultArtifactClient();
+    const downloadResponse = await artifactClient.downloadArtifact(artifactName);
+    const filePath = path.join(downloadResponse.downloadPath, 'jest-results.json');
 
-        if (fs.existsSync(filePath)) {
-            const testResults = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-            console.log('Jest Test Results:', testResults);
-            // Add additional processing of test results here
-        } else {
-            core.setFailed('Test results file not found');
-        }
-    } catch (error) {
-        core.setFailed(`Action failed with error ${error}`);
+    if (fs.existsSync(filePath)) {
+        const testResults = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+        console.log('Jest Test Results:', testResults);
+        // Add additional processing of test results here
+    } else {
+        core.setFailed('Test results file not found');
     }
+    // } catch (error) {
+    core.setFailed(`Action failed with error ${error}`);
+    // }
 }
 
 run();
