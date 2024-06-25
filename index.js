94d134e3505e0a2e05471dbbd7db33d26bdf971f
kamesan1577 <kamesan1577@users.noreply.github.com>
2024-06-26 00:33:06 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const core = require('@actions/core');
+const artifact = require('@actions/artifact');
+const fs = require('fs');
+const path = require('path');
+
+async function run() {
+    try {
+        const artifactName = core.getInput('artifact-name');
+        const artifactClient = artifact.create();
+        const downloadResponse = await artifactClient.downloadArtifact(artifactName);
+        const filePath = path.join(downloadResponse.downloadPath, 'jest-results.json');
+
+        if (fs.existsSync(filePath)) {
+            const testResults = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+            console.log('Jest Test Results:', testResults);
+            // Add additional processing of test results here
+        } else {
+            core.setFailed('Test results file not found');
+        }
+    } catch (error) {
+        core.setFailed(`Action failed with error ${error}`);
+    }
+}
+
+run();
