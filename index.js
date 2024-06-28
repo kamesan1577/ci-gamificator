7672540e46af874b7039d5cbda88c50dc34d7524
kamesan1577 <kamesan1577@users.noreply.github.com>
2024-06-28 12:36:16 +0900

    chore: Update artifact client initialization in index.js

--- index.js
+++ index.js
@@ -2,45 +2,32 @@ const core = require('@actions/core');
 const artifact = require('@actions/artifact');
 const fs = require('fs');
 const path = require('path');
-const AdmZip = require('adm-zip');
 
 async function run() {
-    // try {
-    const artifactName = core.getInput('artifact-name');
-    const artifactClient = new artifact.DefaultArtifactClient();
-    const downloadResponse = await artifactClient.downloadArtifact(artifactName);
-    const zipPath = path.join(downloadResponse.downloadPath, `${artifactName}.zip`);
+    try {
+        const artifactName = core.getInput("artifact-name");
+        const artifactClient = new artifact.DefaultArtifactClient();
+        const downloadPath = path.join(process.cwd(), 'downloaded-artifact');
 
-    if (fs.existsSync(zipPath)) {
-        const zip = new AdmZip(zipPath);
-        const extractedPath = path.join(downloadResponse.downloadPath, "extracted");
-        zip.extractAllTo(extractedPath, true);
-
-        const filePath = path.join(extractedPath, 'test-results.json');
-        if (!fs.existsSync(filePath)) {
-            core.setFailed('Test results file not found');
-            return;
+        if (!fs.existsSync(downloadPath)) {
+            fs.mkdirSync(downloadPath);
         }
 
-        const fileContent = fs.readFileSync(filePath, 'utf8');
-        console.log('File content:', fileContent);
+        console.log(`Downloading artifact ${artifactName} to ${downloadPath}`);
+        const downloadResponse = await artifactClient.downloadArtifact(artifactName, downloadPath);
+        const filePath = path.join(downloadResponse.downloadPath, 'jest-results.json');
 
-        // BigIntの処理を行うためのカスタムパーサーを使用
-        const testResults = JSON.parse(fileContent, (key, value) => {
-            if (typeof value === 'bigint') {
-                return value.toString(); // BigIntを文字列に変換
-            }
-            return value;
-        });
-
-        console.log('Jest Test Results:', testResults);
-        // Add additional processing of test results here
-    } else {
-        core.setFailed('Test results file not found');
+        if (fs.existsSync(filePath)) {
+            const data = fs.readFileSync(filePath, "utf8");
+            const jsonData = JSON.parse(data);
+            console.log(jsonData);
+        } else {
+            console.log(`File not found: ${filePath}`)
+        }
     }
-    // } catch (error) {
-    //     core.setFailed(`Action failed with error ${error}`);
-    // }
+    catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
 run();
